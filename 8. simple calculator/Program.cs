d6d7cd2fbffa843defd529e48d1921fe120b8361
nomvula <nzshabangu95@gmail.com>
2023-09-12 08:34:35 +0200

    Add project files.

---
+++ 8. simple calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.simple_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int answer = num1 + num2;
+
+            Console.WriteLine(num1 + "  + " + num2 + " = " + answer);
+           
+
+            Console.ReadLine();
+        }
+    }
+}
